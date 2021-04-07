48a09d82612254fe7b38b514f18dfbb41b29817d
Kaihao Tian <f0041yt@plank.thayer.dartmouth.edu>
2021-04-07 09:45:51 -0400

    pulled out str2int to separate files.

---
+++ str2int.h
@@ -0,0 +1,9 @@
+bool str2int(const char string[], int * number)
+{
+  // The following is one of my favorite tricks.                                                              
+  // We use sscanf() to parse a number, expecting there to be no following                                    
+  // character ... but if there is, the input is invalid.                                                     
+  // For example, 1234x will be invalid, as would 12.34 or just x.                                            
+  char nextchar;
+  return (sscanf(string, "%d%c", number, &nextchar) == 1);
+}
